c2800fb7b9f69efe6986a97047b7cb4a66f2d608
Botsie696 <75887966+Botsie696@users.noreply.github.com>
2024-11-30 20:24:06 -0800

    Update videoFetcher.py

--- videoFetcher.py
+++ videoFetcher.py
@@ -14,7 +14,7 @@ def get_proxies_from_api():
 
 # Example usage
 proxies = get_proxies_from_api()
-
+# print(proxies)
 
 # Convert proxies to the required format
 def convert_to_proxy_list(proxies):
@@ -39,11 +39,31 @@ def test_proxy(proxy):
             
             return True
     except Exception as e:
-        print(f"Failing proxy  + {proxy}")
+        # print(f"Failing proxy  + {proxy}")
         # print(f"Proxy failed: ==={proxy}=== - {e}")
         pass
     return False
 
+def get_youtube_transcriptOnce(video_url):
+        global ProxyWorks
+        proxy = ProxyWorks
+        if test_proxy(ProxyWorks):
+            try:
+                # Extract video ID from the URL
+                video_id = video_url.split("v=")[-1]
+                # Fetch transcript using the working proxy
+                transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy)
+                # Combine transcript segments into a single string
+                transcript_text = ' '.join([entry['text'] for entry in transcript])
+                
+                return transcript_text
+            except Exception as e:
+                print(f"Failed to retrieve transcript with proxy {proxy}: {e}")
+                # ProxyToggles = False
+                pass
+                 
+        print("All proxies failed.")
+        return None
 def get_youtube_transcript(video_url):
     global ProxyToggles  # Declare ProxyToggles as global to modify its value
     global ProxyWorks    #
@@ -65,16 +85,20 @@ def get_youtube_transcript(video_url):
                 return transcript_text
             except Exception as e:
                 print(f"Failed to retrieve transcript with proxy {proxy}: {e}")
-                ProxyToggles = False
+                # ProxyToggles = False
+                continue
                  
     print("All proxies failed.")
     return None
 
 # https://youtu.be/haDjmBT9tu4?si=K_a1Fu-SaUTnvniE
 # Example usage
-# if __name__ == "__main__":
-#     for n in sites.MainStokksArray: 
-#         video_url = n
-#         # transcript = get_youtube_transcript(video_url)
-#         if transcript:
-#             print(transcript)
+if __name__ == "__main__":
+    transcript = get_youtube_transcript(sites.MainStokksArray[0])
+    print("Next")
+    transcript = get_youtube_transcriptOnce(sites.MainStokksArray[0])
+    # for n in sites.MainStokksArray: 
+    #     video_url = n
+    #     transcript = get_youtube_transcript(video_url)
+    #     if transcript:
+    #         print("transcript received")
