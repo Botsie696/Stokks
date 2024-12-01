c3fb9cec183e06179956c763ff28df4a8383bafb
Botsie696 <75887966+Botsie696@users.noreply.github.com>
2024-11-30 21:57:43 -0800

    Update sites.py

--- sites.py
+++ sites.py
@@ -1,8 +1,5 @@
 from youtube_search import YoutubeSearch
-from fp.fp import FreeProxy
-proxy = FreeProxy(country_id=['US', 'BR']).get()
-print(proxy)
-print("Proxu")
+
 # Debugging: Print the full structure of results
 import pprint
 # 
@@ -10,21 +7,22 @@ import pprint
 # Add according to days that came up today only and yday 
 # 
 # Search for videos
-results = YoutubeSearch('Stocks', max_results=3).to_dict()
+results = YoutubeSearch('Stocks', max_results=5).to_dict()
 # pprint.pprint(results)
 MainStokksArray = []
 for v in results:
     Link = 'https://www.youtube.com' + v['url_suffix']
     MainStokksArray.append(Link)
 
-results = YoutubeSearch('Stocks to buy now', max_results=3).to_dict()
+results = YoutubeSearch('Stocks to buy now', max_results=5).to_dict()
 # pprint.pprint(results)
 for v in results:
     Link = 'https://www.youtube.com' + v['url_suffix']
+    print(v['url_suffix'])
     MainStokksArray.append(Link)
 
 MainStokksArray = list(set(MainStokksArray))
-print(MainStokksArray)
+# print(MainStokksArray)
 
 # results = YoutubeSearch('Top Stocks to buy Now under 50$', max_results=12).to_dict()
 # for v in results:
