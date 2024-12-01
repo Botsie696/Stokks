8c9984f9122cd21beba5116950e7b516a31fe430
Botsie696 <75887966+Botsie696@users.noreply.github.com>
2024-11-30 18:13:31 -0800

    Update videoFetcher.py

--- videoFetcher.py
+++ videoFetcher.py
@@ -5,7 +5,7 @@ import re
 import requests
  
 import requests
-
+ProxyToggles = False
 def get_proxies_from_api():
     url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all"
     response = requests.get(url)
@@ -27,27 +27,30 @@ def convert_to_proxy_list(proxies):
 # Fetch proxies and convert them
 proxies = get_proxies_from_api()
 PROXIES_LIST = convert_to_proxy_list(proxies)
-print(PROXIES_LIST)
+# print(PROXIES_LIST)
 ProxyWorks = ""
-ProxyToggle = False
+
 def test_proxy(proxy):
     try:
         # Test if the proxy works
         response = requests.get("https://www.youtube.com", proxies=proxy, timeout=5)
         if response.status_code == 200:
             print(f"Proxy works: {proxy}")
-            ProxyWorks = proxy
-            ProxyToggle = True
+            
             return True
     except Exception as e:
-        print(f"Proxy failed: ==={proxy}=== - {e}")
+        print(f"Failing proxy  + {proxy}")
+        # print(f"Proxy failed: ==={proxy}=== - {e}")
+        pass
     return False
 
 def get_youtube_transcript(video_url):
+    global ProxyToggles  # Declare ProxyToggles as global to modify its value
+    global ProxyWorks    #
     for proxy in PROXIES_LIST:
-        if ProxyToggle:
+        if ProxyToggles:
             proxy = ProxyWorks
-            print("Proxy Worked" + proxy)
+            
         if test_proxy(proxy):
             try:
                 # Extract video ID from the URL
@@ -56,17 +59,20 @@ def get_youtube_transcript(video_url):
                 transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy)
                 # Combine transcript segments into a single string
                 transcript_text = ' '.join([entry['text'] for entry in transcript])
+                ProxyWorks = proxy
+                ProxyToggles = True
                 return transcript_text
             except Exception as e:
                 print(f"Failed to retrieve transcript with proxy {proxy}: {e}")
+                 
     print("All proxies failed.")
     return None
 
 # https://youtu.be/haDjmBT9tu4?si=K_a1Fu-SaUTnvniE
 # Example usage
-if __name__ == "__main__":
-    for n in sites.MainStokksArray: 
-        video_url = n
-        transcript = get_youtube_transcript(video_url)
-        if transcript:
-            print(transcript)
+# if __name__ == "__main__":
+#     for n in sites.MainStokksArray: 
+#         video_url = n
+#         # transcript = get_youtube_transcript(video_url)
+#         if transcript:
+#             print(transcript)
