c4030acb43ec358506b54d9b709679a6a30a9545
Botsie696 <75887966+Botsie696@users.noreply.github.com>
2024-12-04 21:48:26 -0800

    Update dataprovider.py

--- dataprovider.py
+++ dataprovider.py
@@ -1,5 +1,5 @@
 DepthForScore = 15
-Months = 3
+Months = 4
 NoSites = 9
 Youtubers = ["Felix & Friends (Goat Academy)"  ,"Invest with Henry"  , "Stock Moe"  , "Chris Sain"]
-YoutubersDetailsSites = 3
+YoutubersDetailsSites = 4
