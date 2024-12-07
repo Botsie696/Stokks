17702edb1ffb3e849b88c695688727f4c0f843d1
Botsie696 <75887966+Botsie696@users.noreply.github.com>
2024-12-07 12:15:10 -0800

    Add files via upload

--- dataprovider.py
+++ dataprovider.py
@@ -1,5 +1,5 @@
 DepthForScore = 15
 Months = 4
-NoSites = 4
-Youtubers = ["Felix & Friends (Goat Academy)","Finacial Education" ,"Invest With Corey"  ,"Invest with Henry"  , "Stock Moe"  , "Chris Sain"]
+NoSites = 6
+Youtubers = ["Felix & Friends (Goat Academy)", "FinancialEducation Jeremy Lefebvre " ,"Invest With Corey"  ,"Invest with Henry"  , "Stock Moe"  , "Chris Sain"]
 YoutubersDetailsSites = 3
