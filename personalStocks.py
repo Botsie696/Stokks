03c0b57afc2f4f3ecce313b4f4f80376f09e1ee7
Botsie696 <75887966+Botsie696@users.noreply.github.com>
2025-02-12 20:05:03 -0800

    Update personalStocks.py

--- personalStocks.py
+++ personalStocks.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import betterstokks
 stocks = ["AAL", "TSLA", "NVDA", "SOFI", "ALLT", "KMDA", "BBAI",  "VRME", "RR" , 'PLTR' , 'SOUN' ]
-stocks += ["GRRR", "TARS" , "RCAT" , "SPCB" , "SUPV" , "ACHR" , "LUNR" , "LBRT" , "SMWB" , "HUT" , "KTOS" , "CRK" , "SEI" , "WULF"]
+stocks += ["GRRR", "TARS" , "RCAT" , "ACHR", "KC"  , "PDYN"  , "PL"  , "SOPA" , "HOOD" , "ACHR" , "LUNR" , "LBRT" , "BBAI" , "KMDA" , "HIMS"  ]
 stocks += ["HOOD" , "ESOA" , "AR"]
 file_path = "PersonalStocks.txt"
 betterstokks.WriteToFileAverage(stocks , file_path   ,  months=3)
